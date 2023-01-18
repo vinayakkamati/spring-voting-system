47ed65f03b9c9c0851cff87dbec193a9523e3996
Vinayak kamati <vinayakkamati123@gmail.com>
2023-01-19 00:15:05 +0530

    A unit test case for count vote, list vote, get winner functionalities

--- src/test/java/com/assignment/springvotingsystem/mock/VoteServiceMock.java
+++ src/test/java/com/assignment/springvotingsystem/mock/VoteServiceMock.java
@@ -2,6 +2,9 @@ package com.assignment.springvotingsystem.mock;
 
 import com.assignment.springvotingsystem.models.CandidateDetails;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static com.assignment.springvotingsystem.constants.GenericConstants.NAME;
 
 public class VoteServiceMock {
@@ -9,5 +12,11 @@ public class VoteServiceMock {
         return CandidateDetails.builder()
                 .name(NAME)
                 .vote(0L).build();
+    }
+
+    public static Map<String,Long> getCandidatesDetails() {
+        HashMap<String, Long> candidatesDetails = new HashMap<>();
+        candidatesDetails.put(NAME,3L);
+        return candidatesDetails;
     }
 }
--- src/test/java/com/assignment/springvotingsystem/serviceTest/VoteServiceImplTest.java
+++ src/test/java/com/assignment/springvotingsystem/serviceTest/VoteServiceImplTest.java
@@ -13,11 +13,12 @@ import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 import java.util.HashMap;
+import java.util.Map;
 
 import static com.assignment.springvotingsystem.constants.GenericConstants.NAME;
 import static com.assignment.springvotingsystem.mock.VoteServiceMock.getCandidateDetails;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static com.assignment.springvotingsystem.mock.VoteServiceMock.getCandidatesDetails;
+import static org.junit.jupiter.api.Assertions.*;
 
 class VoteServiceImplTest {
 
@@ -42,7 +43,7 @@ class VoteServiceImplTest {
     }
 
     @Test
-    @DisplayName("Positive case: castVote")
+    @DisplayName("Positive case: cast vote")
     void castVotePositiveCase() {
         try{
             Long expectedValue = 0L;
@@ -55,24 +56,49 @@ class VoteServiceImplTest {
     }
 
     @Test
-    @DisplayName("Negative case: castVote")
+    @DisplayName("Negative case: cast vote")
     void castVote() {
         try{
-            voteService.castVote("Ajay");
+            voteService.castVote(NAME);
         }catch (InvalidRequestParamException e){
             Assertions.assertTrue(true);
         }
     }
 
     @Test
+    @DisplayName("Positive case: count vote")
+    void countVotePositiveCase() {
+        try{
+            Long expectedValue = 2L;
+            Mockito.when(candidatesDetails.containsKey(NAME)).thenReturn(true);
+            Mockito.when(candidatesDetails.get(NAME)).thenReturn(2L);
+            Long actualValue = voteService.countVote(NAME);
+            assertEquals(expectedValue,actualValue);
+        }catch (InvalidRequestParamException ignored){
+        }
+    }
+
+    @Test
+    @DisplayName("Negative case: count vote")
     void countVote() {
+        try{
+            voteService.countVote(NAME);
+        }catch (InvalidRequestParamException e){
+            Assertions.assertTrue(true);
+        }
     }
 
     @Test
     void listVote() {
+        Map<String, Long> expectedValue = new HashMap<>();
+        Map<String, Long> actualValue = voteService.listVote();
+        assertEquals(expectedValue,actualValue);
     }
 
     @Test
     void getWinner() {
+        Mockito.when(candidatesDetails.entrySet()).thenReturn(getCandidatesDetails().entrySet());
+        String actualValue = voteService.getWinner();
+        assertEquals(NAME,actualValue);
     }
 }
